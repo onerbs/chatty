7babe9ca9f8b9e7e3182a5bf97ea72ddc6a69986
AAAA <c326e738e52c5469f518071e@yahoo.com>
2021-03-06 08:47:19 -0600

    lib(theme): Manage the site color scheme

---
+++ src/lib/theme.ts
@@ -0,0 +1,36 @@
+const QUERY = '(prefers-color-scheme: dark)'
+const ROOT = document.documentElement
+
+/** Check if the dark mode is currently active. */
+export function isDarkMode(): boolean {
+  return localStorage.DARK_MODE === 'true'
+}
+
+/**
+ * Toggle the current dark mode status.
+ * @return The new status
+ */
+export function toggleDarkMode(): boolean {
+  const status = !isDarkMode()
+  setDarkMode(status)
+  return status
+}
+
+/**
+ * Initialize the theme status on application startup.
+ * This function is meant to be called once.
+ */
+export function setupTheme() {
+  if (localStorage.DARK_MODE === 'false') return
+  setDarkMode(isDarkMode() || matchMedia(QUERY).matches)
+}
+
+/** Set the dark mode status to the provided one. */
+function setDarkMode(status: boolean): void {
+  localStorage.DARK_MODE = status
+  if (status) {
+    ROOT.setAttribute('data-theme', 'dark')
+  } else {
+    ROOT.removeAttribute('data-theme')
+  }
+}
