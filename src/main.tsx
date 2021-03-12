85ba30738691f10414674f990cf70648cfd35f1b
AAAA <c326e738e52c5469f518071e@yahoo.com>
2021-03-11 21:16:32 -0600

    app(main): Reorganize code

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,17 @@
-import React, { StrictMode } from 'react'
-import { render } from 'react-dom'
+import {detectCurrentTheme} from './lib/theme'
+detectCurrentTheme()
+
+import {isValidSession} from './lib/state'
+console.assert(isValidSession(), 'The session has expired.')
+
+import React, {StrictMode} from 'react'
+import {render} from 'react-dom'
 
 import 'nushi.sass'
 import 'daniela.scss'
 import 'chatty.sass'
 
 import Chatty from './Chatty'
-
-import md5 from './lib/md5'
-import {isValidSession} from './lib/state'
-import {setupTheme} from './lib/theme'
-
-setupTheme()
-
-console.assert(isValidSession(), 'The session has expired.')
-console.assert(
-  md5('hello') == '5d41402abc4b2a76b9719d911017c592',
-  'The MD5 function is not working properly.'
-)
 
 render(
   <StrictMode>
