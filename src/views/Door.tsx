c6b35c20f71ef6234af4dba4a14499044c5a0bd3
AAAA <c326e738e52c5469f518071e@yahoo.com>
2021-03-13 20:16:01 -0600

    view(Door): Display validation info

--- src/views/Door.tsx
+++ src/views/Door.tsx
@@ -1,9 +1,11 @@
-import React, {useEffect, useState} from 'react'
-import {Link, useHistory} from 'react-router-dom'
+import React, { useEffect, useState, useRef } from 'react'
+import { Link, useHistory } from 'react-router-dom'
 import * as validate from '../lib/validators'
 import * as users from '../lib/users'
-import {dispatch} from '../lib/reducer'
-import {Routes} from '../Chatty'
+import { dispatch } from '../lib/reducer'
+import { Routes } from '../Chatty'
+import { modalHTML } from '@onerbs/daniela'
+import Icon from '../parts/Icon'
 import state from '../lib/state'
 
 type props = {
@@ -15,13 +17,19 @@ type props = {
 }
 
 export default function Door({ text }: props) {
-  const history = useHistory()
-  const [disabled, setDisabled] = useState(false)
   const [data, setData] = useState({
-    persistent: state.persistent,
     username: '',
     password: '',
   })
+  const [attempts, setAttempts] = useState({
+    username: 0,
+    password: 0,
+  })
+  const [disabled, setDisabled] = useState(false)
+  const [persistent, setPersistent] = useState(state.persistent)
+  const usernameRef = useRef<HTMLInputElement>(null)
+  const passwordRef = useRef<HTMLInputElement>(null)
+  const history = useHistory()
 
   useEffect(() => {
     if (state.hash) {
@@ -31,64 +39,84 @@ export default function Door({ text }: props) {
 
   function onInputChange(event: any) {
     const { name, value } = event.target
-    setData(prevState => ({ ...prevState, [name]: value }))
+    setData(prev => ({ ...prev, [name]: value }))
+    switch(name) {
+      case 'username':
+        usernameRef.current?.classList.remove('error')
+        break
+      case 'password':
+        passwordRef.current?.classList.remove('error')
+        break
+    }
   }
 
   function onCheckboxChange() {
-    setData(prevState => ({
-      ...prevState,
-      persistent: !prevState.persistent
-    }))
+    setPersistent(!persistent)
   }
 
   async function onSubmit(ev: any) {
     ev.preventDefault()
     const { username, password } = data
-    if (
-      validate.username(username) &&
-      validate.password(password)
-    ) {
-      state.persistent = data.persistent
-      const email = `${username}@chatty.app`
-      const operation = text.route === Routes.SIGN_UP
-        ? users.login
-        : users.signup
-      setDisabled(true)
-      if (await operation(email, password).then(dispatch)) {
-        history.push(Routes.CHAT)
-      } else {
-        setDisabled(false)
-      }
+
+    if (validate.username(username)) {
+      setAttempts(prev => ({ ...prev, username: 0 }))
+      usernameRef.current?.classList.remove('error')
+    } else {
+      setAttempts(prev => ({ ...prev, username: prev.username + 1 }))
+      usernameRef.current?.classList.add('error')
+      return
+    }
+
+    if (validate.password(password)) {
+      setAttempts(prev => ({ ...prev, password: 0 }))
+      passwordRef.current?.classList.remove('error')
+    } else {
+      setAttempts(prev => ({ ...prev, password: prev.password + 1 }))
+      passwordRef.current?.classList.add('error')
+      return
+    }
+
+    state.persistent = persistent
+    const email = `${username}@chatty.app`
+    const operation = text.route === Routes.SIGN_UP
+      ? users.login
+      : users.signup
+    setDisabled(true)
+    if (await operation(email, password).then(dispatch)) {
+      history.push(Routes.CHAT)
+    } else {
+      setDisabled(false)
     }
   }
 
-  // todo: 1) Display validation info.
   return (
     <div className='Door'>
       <form onSubmit={onSubmit}>
         <h1>Chatty</h1>
-        <label>Username {/* 1 */}
+        <label>Username {attempts.username > 0 && <HelpUsername />}
           <input
             type="text"
             name="username"
             value={data.username}
             onChange={onInputChange}
             disabled={disabled}
+            ref={usernameRef}
           />
         </label>
-        <label>Password {/* 1 */}
+        <label>Password {attempts.password > 0 && <HelpPassword />}
           <input
             type="password"
             name="password"
             value={data.password}
             onChange={onInputChange}
             disabled={disabled}
+            ref={passwordRef}
           />
         </label>
         <label>
           <input
             type="checkbox"
-            checked={data.persistent}
+            checked={persistent}
             onChange={onCheckboxChange}
             disabled={disabled}
           />
@@ -98,5 +126,36 @@ export default function Door({ text }: props) {
         <span>or <Link replace to={text.route}>{text.link}</Link></span>
       </form>
     </div>
+  )
+}
+
+function HelpUsername() {
+  return <HelpIcon action={async () => {
+    await modalHTML('A valid username', `
+<ul>
+  <li>Starts with a letter</li>
+  <li>Does not contain white spaces</li>
+  <li>Is at least 4 characters long</li>
+  <li>Contain only lowercase english letters or underscore</li>
+</ul>`)
+  }} />
+}
+
+function HelpPassword() {
+  return <HelpIcon action={async () => {
+    await modalHTML('A valid password', `
+<ul>
+  <li>Is at least 8 characters long</li>
+</ul>`)
+  }} />
+}
+
+function HelpIcon({ action }: { action(): void }) {
+  return (
+    <Icon
+      name="help_outline"
+      title="Get validation info"
+      onClick={() => action()}
+    />
   )
 }
