5f7159aeb2ff9630e887da311854dda9e1dd86f0
AAAA <c326e738e52c5469f518071e@yahoo.com>
2021-03-13 20:16:30 -0600

    part(Avatar): Use another avatar generator

--- src/parts/Avatar.tsx
+++ src/parts/Avatar.tsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import * as React from 'react'
+import { base64Svg } from '@onerbs/ident'
 
 type props = {
-  to: string
-  size?: number
+  value: string,
+  size?: number,
 }
-export default function Avatar({to, size=32}: props) {
-  // Q: Use a local avatar generator?
-  const api = 'https://identicon-api.herokuapp.com'
-  const url = `${api}/${to}/${size}?format=png`
-  const alt = `${to}'s avatar`
-  return <img alt={alt} title={to} src={url}/>
+
+export default function Avatar({ value, size = 24 }: props) {
+  return (
+    <img
+      src={base64Svg(value, size)}
+      alt={`${value}'s avatar`}
+    />
+  )
 }
